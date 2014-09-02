6f0a73448ed440270372deb9d9f41711ecdd2636
Allen Wu <allenwu1018@gmail.com>
2014-09-03 01:27:32 +0800

    tmp

--- app/src/Datepicker.js
+++ app/src/Datepicker.js
@@ -2,50 +2,9 @@ define(function(require, exports, module) {
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
   var Scrollview = require('famous/views/Scrollview');
-  var View = require('famous/core/View');
+  //var View = require('famous/core/View');
   var SequentialLayout = require('famous/views/SequentialLayout');
   var ContainerSurface = require('famous/surfaces/ContainerSurface');
-
-  /**
-   * @class Datepicker
-   * @constructor
-   *
-   */
-  function Datepicker(options) {
-    this.width = options.size && options.size.length ? options.size[0] : 200;
-    this.height = options.size && options.size.length ? options.size[1] : 300;
-    this.scroll = options.scroll ? options.scroll : { direction: 1 };
-
-    var container = new ContainerSurface({
-      size: [this.width, this.height]
-    });
-    var layout = new SequentialLayout({ direction: 0 });
-
-    var yearPicker = new Scrollview(this.scroll);
-    var monthPicker = new Scrollview(this.scroll);
-    var dayPicker = new Scrollview(this.scroll);
-
-    var years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999];
-    var months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
-    var days = [];
-
-    for (var i = 0; i < 31; i++) {
-      days.push(i+1);
-    }
-
-    var yearPicker = new Picker(years, this.width/3, this.height);
-    var monthPicker = new Picker(months, this.width/3, this.height);
-    var dayPicker = new Picker(days, this.width/3, this.height);
-
-    layout.sequenceFrom([yearPicker, monthPicker, dayPicker]);
-    container.add(new Modifier({
-      size: [this.width, this.height]
-    })).add(layout);
-
-    return container;
-  }
-
-  Datepicker.prototype.constructor = Datepicker;
 
   /**
    * @class Picker
@@ -61,16 +20,66 @@ define(function(require, exports, module) {
       }
     });
 
-    var scroll = new Scrollview({ direction: 1 });  
+    var scroll = new Scrollview({ direction: 1, paginated: true });
 
     scroll.sequenceFrom(selections.map(function(selection) {
       // TODO: itemHeight
-      return _selectionItem(selection, width, 20, scroll);
+      return _selectionItem(selection, width, 30, scroll);
     }));
     container.add(scroll);
 
+    scroll.on('pageChange', function() {
+      // TODO: update Day for Year & Month
+      var s = scroll._node.get();
+      window.console.log('current index is: ' + s.getContent());
+    });
+
+    container.scroll = scroll;
+
     return container;
   }
+
+  /**
+   * @class Datepicker
+   * @constructor
+   *
+   */
+  function Datepicker(options) {
+    this.width = (options.size && options.size.length) ? options.size[0] : 200;
+    this.height = (options.size && options.size.length) ? options.size[1] : 300;
+    this.scroll = options.scroll ? options.scroll : { direction: 1 };
+
+    var container = new ContainerSurface({
+      size: [this.width, this.height]
+    });
+    var layout = new SequentialLayout({ direction: 0 });
+
+    var years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999];
+    var months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
+    var days = [];
+
+    for (var i = 0; i < 31; i++) {
+      days.push(i+1);
+    }
+
+    var yearPicker = new Picker(years, this.width/3, this.height);
+    var monthPicker = new Picker(months, this.width/3, this.height);
+    var dayPicker = new Picker(days, this.width/3, this.height);
+
+    window.yearPicker = yearPicker;
+    window.monthPicker = monthPicker;
+    window.dayPicker = dayPicker;
+
+    layout.sequenceFrom([yearPicker, monthPicker, dayPicker]);
+    container.add(new Modifier({
+      size: [this.width, this.height]
+    })).add(layout);
+
+    return container;
+  }
+
+  Datepicker.prototype.constructor = Datepicker;
+
   /**
    * @private
    *
@@ -81,7 +90,8 @@ define(function(require, exports, module) {
       content: content,
       size: [width, height],
       properties: {
-        textAlign: 'center'
+        textAlign: 'center',
+        lineHeight: height + 'px'
       }
     });
     s.pipe(scroll);
